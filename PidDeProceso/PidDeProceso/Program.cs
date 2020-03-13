2d9cdad1437207cabaab1c5aee6f441c794ee35d
bet160 <36347319+bet160@users.noreply.github.com>
2020-03-13 16:51:17 -0600

    Proyecto en c#
    Carpeta con los archivos de la solución.

---
+++ PidDeProceso/PidDeProceso/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PidDeProceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process proceso = Process.GetCurrentProcess();
+            Console.WriteLine(proceso.Id);
+            Console.ReadLine();
+        }
+    }
+}
